d88318533ca509ee1a998e8ca6d6015c8b4b8e5f
Navyata Bawa <navvubawa@gmail.com>
2017-04-29 11:35:29 -0700

    Draw line on monochrome screen

---
+++ src/monoChromeScreen.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+#include <vector>
+using namespace std;
+
+void drawLine(char* screen, int width, int x1, int x2,int y) {
+	int firstFullByte = x1 / 8;
+	int startOffset = x1 % 8;
+	if (startOffset != 0)
+		firstFullByte++;
+
+	int lastFullByte = x1 / 8;
+	int endOffset = x1 % 8;
+	if (endOffset != 0)
+		lastFullByte++;
+
+	//set pixels for full bytes
+	for (int b = firstFullByte; b < lastFullByte; b++)
+		screen[(width / 8)*y + b] = 0xFF;
+
+	char startMask = 0xFF >> startOffset;
+	char endMask = ~(0xFF >> endOffset+1);
+
+	//if both x1 and x2 are in same byte
+	if ((x1 % 8) == (x2 % 8)) {
+		char mask = startMask & endMask;
+		screen[(width / 8)*y + x1] |= mask;
+	}
+	else {
+		if (startOffset != 0) {
+			char num = (width / 8)*y + firstFullByte - 1;
+			screen[num] |= startMask;
+		}
+		if (endOffset != 7) {
+			char num = (width / 8)*y + lastFullByte + 1;
+			screen[num] |=endMask;
+		}
+	}
+}
+
+int main() {
+
+	return 0;
+}
